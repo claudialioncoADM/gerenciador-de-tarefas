78d5a1630a87289829f84c5dfc0c0d1a2af1ba41
claudialioncoADM <claudialionco@gmail.com>
2025-08-17 15:30:08 -0300

    gerenciador

---
+++ src/app/tarefas/editar/editar-tarefa.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-tarefa',
+  standalone: false,
+  templateUrl: './editar-tarefa.component.html',
+  styleUrl: './editar-tarefa.component.css'
+})
+export class EditarTarefaComponent {
+
+}
--- src/app/tarefas/index.ts
+++ src/app/tarefas/index.ts
@@ -3,4 +3,5 @@ export * from './shared';
 export * from './listar';
 export * from './tarefas-routing.module';
 export * from './cadastrar';
+export * from './editar';
 
